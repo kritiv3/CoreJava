fb8712e898880523343e67685d98113c0ad4a2e0
Kriti Verma <kritiv7300@gmail.com>
2024-03-03 17:39:42 +0530

    changes successfully done

---
+++ 01JavaBasicCode/simplePrintStatements.java
@@ -0,0 +1,12 @@
+//simple Print Statement
+public class simplePrintStatements{
+    public static void main(String[] args){
+        
+        System.out.print("the cursor will be shown in the same line after printing the required output on the screen--->");
+
+        System.out.println("the cursor will be shown in the next line after printing the required output on the screen--->");
+
+        System.out.print("observe the result");
+
+    }
+}
